a71dd787d3c05b749dc12529742ed55e92bff89f
galet0 <galeto.miteva@gmail.com>
2022-03-04 10:26:23 +0200

    Add project files.

---
+++ BCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
