d6761b72ee66eae676c5b0c689632efdbdfb05c5
Rosen1211 <terca12112004@abv.bg>
2022-03-04 10:49:29 +0200

    name property is added to product entity

---
+++ BCS/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Product
+{
+	public Product()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+
+	}
+    private int name;
+
+    public string Name
+    {
+        get { return name; }
+        private set { name = value; }
+    }
+
+}
