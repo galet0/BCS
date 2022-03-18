e5fb434bb71131c7c8a9889feee8d5dd1ffc7b19
galet0 <galeto.miteva@gmail.com>
2022-03-18 10:05:51 +0200

    adde company entity with properties and constructor

---
+++ BCS/Company.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCS
+{
+    public class Company
+    {
+        private string name;
+        private List<Building> buildings;
+        private List<Broker> brokers;
+
+        public Company(string name)
+        {
+            this.Name = name;
+            this.buildings = new List<Building>();
+            this.brokers = new List<Broker>();
+        }
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+    }
+}
